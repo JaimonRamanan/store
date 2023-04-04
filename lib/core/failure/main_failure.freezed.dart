a0916e5e68039c60752e6d07b2777c00a3ac3fd7
cntc0019 <cntc0019@gmail.com>
2023-04-04 16:24:47 +0530

    failure added

---
+++ lib/core/failure/main_failure.freezed.dart
@@ -0,0 +1,76 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'main_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$MainFailure {}
+
+/// @nodoc
+abstract class $MainFailureCopyWith<$Res> {
+  factory $MainFailureCopyWith(
+          MainFailure value, $Res Function(MainFailure) then) =
+      _$MainFailureCopyWithImpl<$Res, MainFailure>;
+}
+
+/// @nodoc
+class _$MainFailureCopyWithImpl<$Res, $Val extends MainFailure>
+    implements $MainFailureCopyWith<$Res> {
+  _$MainFailureCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$_MainFailureCopyWith<$Res> {
+  factory _$$_MainFailureCopyWith(
+          _$_MainFailure value, $Res Function(_$_MainFailure) then) =
+      __$$_MainFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_MainFailureCopyWithImpl<$Res>
+    extends _$MainFailureCopyWithImpl<$Res, _$_MainFailure>
+    implements _$$_MainFailureCopyWith<$Res> {
+  __$$_MainFailureCopyWithImpl(
+      _$_MainFailure _value, $Res Function(_$_MainFailure) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_MainFailure implements _MainFailure {
+  const _$_MainFailure();
+
+  @override
+  String toString() {
+    return 'MainFailure()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_MainFailure);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _MainFailure implements MainFailure {
+  const factory _MainFailure() = _$_MainFailure;
+}
